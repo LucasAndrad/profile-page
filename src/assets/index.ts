ba7e0bf2c88b2645142ef3136dd665c5415acc8c
Lucas Andrade <llucasandradd@gmail.com>
2021-05-27 10:17:12 -0300

    Add some logo assets

---
+++ src/assets/index.ts
@@ -0,0 +1,9 @@
+import githubLogo from './github-logo.svg';
+import linkedinLogo from './linkedin-logo.svg';
+import twitterLogo from './twitter-logo.svg';
+
+export {
+  githubLogo,
+  linkedinLogo,
+  twitterLogo,
+}
